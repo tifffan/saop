238370c41bae0630689fc9ddab1ac94f3827fd5b
dshuman1 <dshuman1@macalester.edu>
2018-05-30 09:42:18 -0500

    added eval_pi

---
+++ MATLAB_Code/gLf/eval_pi.m
@@ -0,0 +1,12 @@
+function [ Pi ] = eval_pi(ab,absc)
+
+Ncap=length(absc);
+N=size(ab,1);
+Pi=zeros(Ncap,N);
+Pi(:,1)=ones(Ncap,1);
+Pi(:,2)=(absc-ab(1,1)).*Pi(:,1);
+for i=3:N
+    Pi(:,i)=(absc-ab(i-1,1)).*Pi(:,i-1)-ab(i-1,2).*Pi(:,i-2);
+end
+end
+
