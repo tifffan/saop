9415fd4356ccd20644ee54923404ec1cf17a2029
dshuman1 <dshuman1@macalester.edu>
2018-08-21 11:21:00 -0500

    added old folder

---
+++ MATLAB_Code/Old/gsp_cheby_opX.m
@@ -0,0 +1,18 @@
+function [r] = gsp_cheby_opX(G, coeffs)
+
+% Computes 1/2 \alpha_0 \bar{T}_0(L)X + \sum_{k=1}^K \alpha_k
+% \bar{T}_k(L)X, an approximation of h(L)X for some filter h
+	
+if ~isfield(G,'TkbarLX') || ~isfield(G,'X')
+   G=gsp_compute_TkbarLX(G);
+end
+
+num_vec = size(G.X, 2);
+spI = speye(num_vec);
+coeffs(1) = 1/2*coeffs(1);
+coeffs_matrix = kron(coeffs, spI);
+
+r = G.TkbarLX(:,1:size(coeffs_matrix,1))*coeffs_matrix;
+
+end
+
